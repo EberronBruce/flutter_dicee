38b6bf33eab72eb1132aec3ca87c7812d6fc08d1
Bruce <bruce@redravencomputing.com>
2025-06-15 21:34:55 -0500

    init commit

---
+++ android/app/src/main/kotlin/com/redravencomputing/com/flutter_dicee/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.redravencomputing.com.flutter_dicee
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          title: Text('Dicee', style: TextStyle(color: Colors.white)),
+          backgroundColor: Colors.red,
+        ),
+        body: DicePage(),
+      ),
+    ),
+  );
+}
+
+class DicePage extends StatelessWidget {
+  const DicePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                if (kDebugMode) {
+                  print('Left button got pressed.');
+                }
+              },
+              child: Image.asset('images/dice1.png'),
+            ),
+          ),
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                if (kDebugMode) {
+                  print('Right button got pressed.');
+                }
+              },
+              child: Image.asset('images/dice1.png'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
